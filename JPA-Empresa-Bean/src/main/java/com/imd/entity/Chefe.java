6af835085f6cdf8224a3c7e4a0645f2394add148
Franklin Matheus <franklinmatheus1@gmail.com>
2018-05-17 13:12:26 -0300

    updating entity

--- JPA-Empresa-Bean/src/main/java/com/imd/entity/Chefe.java
+++ JPA-Empresa-Bean/src/main/java/com/imd/entity/Chefe.java
@@ -6,6 +6,7 @@
 package com.imd.entity;
 
 import java.io.Serializable;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
